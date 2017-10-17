6d0453cf8b6e49dc074478862edf58009dedb27d
Sysoev Stanislav <SysoevSO@dtek.com>
2017-10-17 16:34:17 +0300

    hardcodded amounts, working add buttons

--- main.js
+++ main.js
@@ -1,7 +1,3 @@
- /**
-     * You must include the dependency on 'ngMaterial' 
-     */
-   
 
     angular.module('progressLinearDemo1', ['ngMaterial'])
   .config(function($mdThemingProvider) {
@@ -12,13 +8,13 @@
     self.determinateValue = 30; 
 
     $scope.auditors = [{name:"Shulyatskiy", amount:0}, {name:"Yazykovskij",amount:0},{name:"Melnikov",amount:0},{name:"Starokon",amount:0},{name:"Olijnyk",amount:0},{name:"Karmanovska",amount:0},{name:"Taran",amount:0}];
-
+	$scope.novator = {knsAmount: {amount:0}, suisAmount: {amount:0}};
+	$scope.odos= {amount:0};
 
     $scope.shul = 10;  
     $scope.increment = function(arg){
-      console.log(arg);
-      arg++; 
-      return;  
+      console.log(arg + " before");
+      arg.amount++; 
     }
 
   }]);
