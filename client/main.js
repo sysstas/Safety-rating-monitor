d3b2804e2254d4cda855cc94c2d77d4357571cb0
Sysoev Stanislav <SysoevSO@dtek.com>
2017-10-19 16:17:53 +0300

    bind statusbar with buttons

--- client/main.js
+++ client/main.js
@@ -4,8 +4,14 @@
   })
   .controller('AppCtrl', ['$scope', '$interval', function($scope, $interval) {
     var self = this;
-
-    self.determinateValue = 30; 
+	let percentage = {};
+	 percentage.audits = 0;
+	 percentage.novator = 0;
+	 percentage.odos = 0;
+	function update(){
+		self.determinateValue = percentage.audits + percentage.novator + percentage.odos; 
+	};
+    update();
 
     $scope.auditors = [{name:"Shulyatskiy", amount:0}, {name:"Yazykovskij",amount:0},{name:"Melnikov",amount:0},{name:"Starokon",amount:0},{name:"Olijnyk",amount:0},{name:"Karmanovska",amount:0},{name:"Taran",amount:0}];
 	$scope.novator = {knsAmount: {amount:0}, suisAmount: {amount:0}};
@@ -15,7 +21,8 @@
     $scope.increment = function(arg){
       console.log(arg + " before");
       arg.amount++; 
-		
+		percentage.audits++;
+		update();
     }
 
   }]);
